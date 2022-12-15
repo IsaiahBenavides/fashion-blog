f5ea88810852ec94e09fa6b8a692e53fce663682
Isaiah Bens <benavidesisaiah@outlook.com>
2022-12-15 16:10:28 -0500

    footer and last of CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import './App.css';
 import Article from './components/Article';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Header />
         <Nav />
         <Article date='11/12/20' title='On the Street in Brooklyn' imageLink='https://ps-rtt-sei.herokuapp.com/fashionblog/blog-image-1.jpg' />
+        <Article date='11/11/20' title='Vintage in Vogue' imageLink='https://ps-rtt-sei.herokuapp.com/fashionblog/blog-image-2.jpg' />
+        <Footer />
       </section>
     </>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+export default function Footer() {
+    return (
+        <footer>
+            <nav>
+                <ul id="footerNav" aria-label="Main Navigation" role="navigation">
+                    <li>
+                        <a href="#">Women's</a>
+                    </li>
+                    <li>
+                        <a href="#">Men's</a>
+                    </li>
+                    <li>
+                        <a href="#">On the Street</a>
+                    </li>
+                    <li>
+                        <a href="#"> The Catwalk</a>
+                    </li>
+                    <li>
+                        <a href="#">AdWatch</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Tips</a>
+                    </li>
+                </ul>
+            </nav>
+            <h5>© 2013 Valet Industries, Inc</h5>
+        </footer>
+    )
+}
